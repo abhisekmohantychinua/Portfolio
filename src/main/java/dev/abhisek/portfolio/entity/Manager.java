a7b42cb1952a555cbb9b8e6262b46e0a43a4864c
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:50:51 +0530

    Managers of my website

---
+++ src/main/java/dev/abhisek/portfolio/entity/Manager.java
@@ -0,0 +1,55 @@
+package dev.abhisek.portfolio.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "managers")
+public class Manager implements UserDetails {
+    private String id;
+    private String email;
+    private String password;
+    private List<String> roles;
+    private boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
