e80feeab3d1402e98f088b615dcc039620206f35
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:53:56 +0530

    project model

---
+++ src/main/java/dev/abhisek/portfolio/model/Project.java
@@ -0,0 +1,27 @@
+package dev.abhisek.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "projects")
+public class Project {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private List<String> technologies;
+    private String imageUrl;
+    private String githubUrl;
+
+
+}
