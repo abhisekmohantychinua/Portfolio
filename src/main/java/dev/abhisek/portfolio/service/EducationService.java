09b18711ae3ac1aecfb2dc92d623c894c1ce4856
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:44:50 +0530

    education service interface

---
+++ src/main/java/dev/abhisek/portfolio/service/EducationService.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.service;
+
+import dev.abhisek.portfolio.model.Education;
+
+import java.util.List;
+
+public interface EducationService {
+    List<Education> getAllEducation();
+}
