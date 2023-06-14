46cf504508928442f3d649cc3e0ef3cd7cfeda5c
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:57:16 +0530

    skill service interface

---
+++ src/main/java/dev/abhisek/portfolio/service/SkillService.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.service;
+
+import dev.abhisek.portfolio.model.Skill;
+
+import java.util.List;
+
+public interface SkillService {
+    List<List<Skill>> getAllSkill();
+}
