a0dfc9691324790f915c235218c8dd1387841a31
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:54:15 +0530

    project service

---
+++ src/main/java/dev/abhisek/portfolio/service/ProjectService.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.service;
+
+import dev.abhisek.portfolio.model.Project;
+
+import java.util.List;
+
+public interface ProjectService {
+    List<Project> getAllProject();
+}
