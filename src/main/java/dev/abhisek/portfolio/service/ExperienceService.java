959caef180576a6271ef36ab3617fc72bf365ff4
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:46:06 +0530

    experience service interface

---
+++ src/main/java/dev/abhisek/portfolio/service/ExperienceService.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.service;
+
+import dev.abhisek.portfolio.model.Experience;
+
+import java.util.List;
+
+public interface ExperienceService {
+    List<Experience> getAllExperience();
+}
