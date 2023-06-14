4075cc1e7e4de6cd2e0c4e8de70b059db0b4df24
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:47:15 +0530

    feedback repository

---
+++ src/main/java/dev/abhisek/portfolio/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.repository;
+
+import dev.abhisek.portfolio.model.Feedback;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends MongoRepository<Feedback, String> {
+}
