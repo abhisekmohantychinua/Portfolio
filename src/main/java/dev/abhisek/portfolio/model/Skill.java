6a4c2078dd4797fccb398857128f90788e8f46a3
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:56:47 +0530

    skill model

---
+++ src/main/java/dev/abhisek/portfolio/model/Skill.java
@@ -0,0 +1,19 @@
+package dev.abhisek.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "skills")
+public class Skill {
+    @Id
+    private String id;
+    private String name;
+}
