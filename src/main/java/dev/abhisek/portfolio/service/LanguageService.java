209a063fe8851b8bf9d3d132ca786c2195437dff
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:49:44 +0530

    language service interface

---
+++ src/main/java/dev/abhisek/portfolio/service/LanguageService.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.service;
+
+import dev.abhisek.portfolio.model.Language;
+
+import java.util.List;
+
+public interface LanguageService {
+    List<List<Language>> getAllLanguage();
+}
