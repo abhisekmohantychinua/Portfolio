c61cb83d7aee82b8b432454d2312f4a1f35cf2eb
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:41:17 +0530

    Authorized controllers for Managers

---
+++ src/main/java/dev/abhisek/portfolio/controller/AuthController.java
@@ -0,0 +1,12 @@
+package dev.abhisek.portfolio.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    // TODO: 13-06-2023 Manager dashboard controllers
+}
