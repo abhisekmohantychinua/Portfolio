71a4cf71dfa2f15df549e0db83c07d74e57d735d
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:55:23 +0530

    security configuration of my app

---
+++ src/main/java/dev/abhisek/portfolio/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package dev.abhisek.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/auth/**", "/actuator/**")
+                            .authenticated()
+                            .anyRequest()
+                            .permitAll();
+                })
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+}
