535906b8ab5de76309a40f302a4996816955a361
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-14 08:49:31 +0530

    language repository

---
+++ src/main/java/dev/abhisek/portfolio/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package dev.abhisek.portfolio.repository;
+
+import dev.abhisek.portfolio.model.Language;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends MongoRepository<Language, String> {
+}
